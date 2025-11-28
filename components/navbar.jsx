e0a6fdaefd56729b1667edaabcccad8f59448c76
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2025-11-28 10:17:32 +0200

    fix: logout issue

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -27,8 +27,14 @@ export function Navbar() {
   }, [])
 
   const handleLogout = async () => {
-    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
-    document.cookie = "isLoggedIn=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+   await fetch("/api/auth/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+   })
+   document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+   document.cookie = "isLoggedIn=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
     window.location.href = "/login"
   }
 
